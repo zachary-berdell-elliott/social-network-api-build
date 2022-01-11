2cda118dc4f0ce3571241910f5f7e9f0a3df18aa
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-11 11:12:47 -0700

    Though model started.

---
+++ models/Thought.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+)
