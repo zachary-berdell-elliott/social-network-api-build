4b355c1bbc10d43d275228f082ed2ab01bf90c6e
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-13 17:49:15 -0700

    User controller has been built.

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,5 +17,37 @@ module.exports = {
             .then(user => res.json(user))
             .catch(err => res.status(500).json(err));
     },
-    
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+            .then(user => !user ? res.status(404).json({message: "This user does not exist."}) : res.json(user))
+            .catch(err => res.status(500).json(err));
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.userId})
+            .then(user => !user ? res.status(404).json({message: "This user does not exist"}): Thought.deleteMany({_id: {$in: user.thoughts}}))
+            .then(() => res.json({message: "The user and associated thoughts have been deleted."}))
+            .catch(err => res.staus(500).json(err));
+    },
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$addToSet: {friends: req.body}},
+            {runValidators: true, new: true}
+        )
+            .then(user => !user ? res.status(404).json({message: "This user does not exist"}) : res.json(user))
+            .catch(err => res.status(500).json(err));
+    },
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$pull: {friend: {friendId: req.params.friendId}}},
+            {runValidators: true, new: true}
+        )
+            .then(user => !user ? res.status(404).json({message: "This user does not exist"}) : res.json(user))
+            .catch(err => res.status(500).json(err));
+    }
 }
