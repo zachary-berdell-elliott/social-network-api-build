9261eab4e886a01d47c98307a593f6bbc8fe51dc
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-14 11:36:24 -0700

    Thought routes have been built.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+module.exports = router;
