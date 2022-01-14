f3cf62130565dbd7db80c4204c3d9f6477c62335
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-14 09:59:20 -0700

    Thought controller almost finished.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,5 +11,26 @@ module.exports = {
             .select('-__v')
             .then(thought => !thought ? res.status(404).json({message: "This thought does not exist."}) : res.json(thought))
             .catch(err => res.status(500).json(err));
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then(thought => res.json(thought))
+            .catch(err => res.status(500).json(err));
+    },
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+            .then(thought => {
+                !thought ? res.status(404).json({message: "This thought does not exist."}) : res.json(thought)
+            })
+            .catch(err => res.status(500).json(err));
+    },
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({_id: req.params.thoughtId})
+            .then(thought => !thought ? res.status(404).json({message: "This thought does not exist."}) : res.json(thought))
+            .catch(err => res.status(500).json(err));
     }
 }
